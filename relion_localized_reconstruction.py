39dfbec1a3896b1f4ff79aa4317ab0563b124d16
Juha Huiskonen <juha@strubi.ox.ac.uk>
2016-07-25 15:36:21 +0100

    Modified usage, grouped different parameters and formatted defaults

--- relion_localized_reconstruction.py
+++ relion_localized_reconstruction.py
@@ -44,24 +44,28 @@ class LocalizedReconstruction():
         self.parser = argparse.ArgumentParser(
             formatter_class=argparse.RawDescriptionHelpFormatter,
             description=textwrap.dedent('''\
-                    Localized reconstruction of subparticles. Normally the script is run in three steps:
+                    Localized reconstruction of subparticles. Normally the script is run in four steps:
                         1. Prepare particles.
                         2. Create subparticles.
                         3. Extract subparticles.
                         4. Reconstruct subparticles.
             '''))
+
+        # Parameter groups
         general = self.parser.add_argument_group('General parameters')
         steps = self.parser.add_argument_group('Steps', 'Several steps can be combined in one run.')
         prepareParticles = self.parser.add_argument_group('Prepare particles')
         createSubparticles = self.parser.add_argument_group('Create subparticles')
         extractSubparticles = self.parser.add_argument_group('Extract subparticles')
         reconstructSubparticles = self.parser.add_argument_group('Reconstruct subparticles')
-        add = general.add_argument()
-        adds = steps.add_argument()
-        addpp = prepareParticles.add_argument()
-        addcs = createSubparticles.add_argument()
-        addes = extractSubparticles.add_argument()
-        addrs = reconstructSubparticles.add_argument()
+
+        # Shortcuts
+        add = general.add_argument
+        adds = steps.add_argument
+        addpp = prepareParticles.add_argument
+        addcs = createSubparticles.add_argument
+        addes = extractSubparticles.add_argument
+        addrs = reconstructSubparticles.add_argument
 
         # General parameters
         add('input_star', help="Input STAR filename with particles.")
@@ -71,11 +75,11 @@ class LocalizedReconstruction():
         # Parameters for "Steps" group
         adds('--prepare_particles', action='store_true',
               help="Prepare particles for extracting subparticles.")
-        addcs('--create_subparticles', action='store_true',
+        adds('--create_subparticles', action='store_true',
               help="Calculate the cooridnates and Euler angles for the subparticles.")
         adds('--extract_subparticles', action='store_true',
               help="Extract subparticles from particle images.")
-        addrs('--reconstruct_subparticles', action='store_true',
+        adds('--reconstruct_subparticles', action='store_true',
               help="Calculate a reconstruction of the subunit from the subparticles. "
                  "Subparticle coordinates are read from "
                  "[output]_subparticles.star and [output]_subparticles_subtracted.star")
@@ -85,26 +89,24 @@ class LocalizedReconstruction():
               help="Create another set of particles with partial signal subtraction using this map.")
 
         # Parameters for "Create subparticles" group
-        addcs('--angpix', type=float, default=1, help="Pixel size (A; default 1 A).")
-        addcs('--sym', help="Symmetry of the particle.")
+        addcs('--angpix', type=float, default=1, help="Pixel size (A). (default: 1)")
+        addcs('--sym', default="C1", help="Symmetry of the particle. (default: C1)")
         addcs('--particle_size', type=int,
               help="Size of the particle box (pixels).")
         addcs('--randomize', action='store_true',
-              help="Randomize the order of the symmetry matrices. \n"
-                   "Useful for preventing preferred orientations (default: not).")
+              help="Randomize the order of the symmetry matrices. "
+                   "Useful for preventing preferred orientations.")
         addcs('--relax_symmetry', action='store_true',
-              help="Create one random subparticle for each particle "
-                   "(default: all symmetry related subparticles).")
-        addcs('--vector', help="Vector defining the location of the subparticle.")
+              help="Create one random subparticle for each particle ")
+        addcs('--vector', default="0,0,1", help="Vector defining the location of the subparticle. (default: 0,0,1)")
         addcs('--align_subparticles', action='store_true',
               help="Align subparticles to the standard orientation.")
         addcs('--length',
               help="Alternative length of the vector. Use to adjust the "
-                 "subparticle center (default: length of the given "
-                 "vector; A).")
+                 "subparticle center (A). (default: length of the given vector)")
         addcs('--cmm',
               help="A CMM file defining the location(s) of the subparticle(s) "
-                 "(use instead of --vector). Coordinates in Angstrom.")
+                 "(use instead of --vector). Coordinates should be in Angstrom.")
         addcs('--unique', type=float, default=-1,
               help="Keep only unique subparticles within angular distance "
                  "(useful to remove overlapping subparticles on symmetry axis).")
@@ -121,12 +123,12 @@ class LocalizedReconstruction():
         # Parameters for "Extract subparticles" group
         addes('--subparticle_size', type=int, required=True,
             help="Size of the subparticle box (pixels).")
-        addes('--np', type=int, default=1, help="Number of MPI procs (default: 1).")
+        addes('--np', type=int, default=1, help="Number of MPI procs. (default: 1)")
 
         # Parameters for "Reconstruct subparticles" group
-        addrs('--j', type=int, default=8, help="Number of threads.")
-        addrs('--maxres', type=float, help="Maximum resolution of the reconstruction (A).")
-        addrs('--subsym', help="Symmetry of the subparticle.")
+        addrs('--j', type=int, default=1, help="Number of threads.")
+        addrs('--maxres', type=float, help="Maximum resolution of the reconstruction (A). (default: Nyquist)")
+        addrs('--subsym', default="C1", help="Symmetry of the subparticle. (default: C1)")
 
     def usage(self):
         self.parser.print_help()
